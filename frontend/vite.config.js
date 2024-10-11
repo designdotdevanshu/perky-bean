e3956242ed4423e79fe468d3264b7ca620da4fad
Devanshu <design.devanshu@gmail.com>
2024-10-11 12:18:40 +0530

    Update file paths for frontend scripts and styles

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  preview: {
-    port: 3000,
-  },
   server: {
-    port: 3000,
     proxy: {
       "/api": {
-        target: "https://perky-bean-server.vercel.app",
+        target: "https://perky-bean-backend.vercel.app",
         changeOrigin: true,
         secure: false,
         ws: true,
